cbb134495e1a5960e9950e29da89779f1b6a89b5
cheeseburgertony <1821955020@qq.com>
2024-07-10 21:05:50 +0800

    feat:Home-ECharts图表基础渲染

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,38 @@
+import * as echarts from 'echarts';
+import { useEffect, useRef } from 'react';
+
 const Home = () => {
-  return <div>this is Home</div>
+  // 1.获取渲染图表的dom节点
+  const chartRef = useRef(null)
+
+  // 保证dom可用(渲染完毕后调用) 才进行图表的渲染
+  useEffect(() => {
+    const chartDom = chartRef.current
+
+    // 2.图表初始化生成图表实例对象
+    const myChart = echarts.init(chartDom);
+    // 3.准备图表参数
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Vue','React','Angular']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [100, 100, 100],
+          type: 'bar'
+        }
+      ]
+    };
+
+    // 4.使用图表参数进行图表渲染
+    option && myChart.setOption(option);
+  }, [])
+
+  return <div><div ref={chartRef} style={{ width: '500px', height: '400px' }}></div></div>
 }
 
 export default Home
