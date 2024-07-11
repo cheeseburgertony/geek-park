9bca11968969d5a45a24a83bc6a8a1e5caf3bbfd
cheeseburgertony <1821955020@qq.com>
2024-07-11 10:24:52 +0800

    feat:发布文章-实现切换封面类型

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -52,6 +52,12 @@ const Publish = () => {
     setImageList(value.fileList)
   }
 
+  // 图片类型切换
+  const [imageType, setImageType] = useState(0)
+  const onTypeChange = (e) => {
+    setImageType(e.target.value)
+  }
+
   return (
     <div className="publish">
       <Card
@@ -66,7 +72,7 @@ const Publish = () => {
         <Form
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
-          initialValues={{ type: 1 }}
+          initialValues={{ type: 0 }}
           onFinish={onFinish}
         >
           <Form.Item
@@ -87,7 +93,7 @@ const Publish = () => {
           </Form.Item>
           <Form.Item label="封面">
             <Form.Item name="type">
-              <Radio.Group>
+              <Radio.Group onChange={onTypeChange}>
                 <Radio value={1}>单图</Radio>
                 <Radio value={3}>三图</Radio>
                 <Radio value={0}>无图</Radio>
@@ -100,7 +106,7 @@ const Publish = () => {
               name:接口要求的字段名
               onChange:在事件中拿到当前图片数据 上传过程中会一直触发
             */}
-            <Upload
+            {imageType > 0 && <Upload
               listType="picture-card"
               showUploadList
               action={'http://geek.itheima.net/v1_0/upload'}
@@ -110,7 +116,7 @@ const Publish = () => {
               <div style={{ marginTop: 8 }}>
                 <PlusOutlined />
               </div>
-            </Upload>
+            </Upload>}
           </Form.Item>
           <Form.Item
             label="内容"
